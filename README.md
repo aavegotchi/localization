4b6a3d7919b3fa288ffef8aab77b727679bd9e57
candoizo <candoizo@protonmail.com>
2021-06-10 00:00:03 -0400

    :passport_control: preparing to read project

---
+++ README.md
@@ -0,0 +1,10 @@
+# aavegotchi-localization
+
+Start with
+
+```sh
+git clone https://github.com/aavegotchi/localization aavegotchi-localization
+cd *localization/
+yarn
+yarn read
+```
---
+++ scripts/read.ts
@@ -0,0 +1,20 @@
+import debug from "debug";
+const log = debug(`*`);
+import secret from "../secret.json";
+
+import crowdin, { Credentials } from '@crowdin/crowdin-api-client';
+const credentials: Credentials = {
+  token: secret.crowdin_api,
+}
+const { projectsGroupsApi } = new crowdin(credentials);
+
+const main = async () => {
+  try {
+    const data = await projectsGroupsApi.listProjects();
+    log(data);
+  } catch(e) {
+    log(`Error: `, e);
+  }
+};
+
+main();
